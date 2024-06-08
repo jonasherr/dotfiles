28b25dbe551f6817074b3a4731172d211543a2a4
Jonas Herrmannsdörfer <jonas@herrmannsdoerfer.dev>
2024-06-08 17:08:05 +0200

    tmux adjusted

--- karabiner/rules/simultaneous.ts
+++ karabiner/rules/simultaneous.ts
@@ -4,7 +4,7 @@ import { createCombinedKey } from "../utils";
 export const simultaneous: KarabinerRules = {
   description: "Simultaneous Keys",
   manipulators: [
-    createCombinedKey(["r", "t"], "delete_or_backspace"),
-    createCombinedKey(["y", "u"], "return_or_enter"),
+    createCombinedKey(["e", "r"], "delete_or_backspace"),
+    createCombinedKey(["u", "i"], "return_or_enter"),
   ].flat()
 }
