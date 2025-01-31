5c688b220b7b82b7022c2f692328a9cb48dbcbd7
Jonas Herrmannsdörfer <jonas@herrmannsdoerfer.dev>
2025-01-31 16:42:14 +0100

    added kitty

---
+++ kitty/install.sh
@@ -0,0 +1,3 @@
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+
+# The binaries will be installed in the standard location for your OS, /Applications/kitty.app on macOS and ~/.local/kitty.app on Linux. The installer only touches files in that directory. To update kitty, simply re-run the command.
