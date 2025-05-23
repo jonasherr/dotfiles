148d4be0246c658a72a64d5aa8232108e0b6de7e
Jonas Herrmannsdörfer <jonas@herrmannsdoerfer.dev>
2025-05-23 18:42:32 +0200

    added image clipping and rendering support in nvim

---
+++ nvim/lua/plugins/files/image-clip.lua
@@ -0,0 +1,22 @@
+return {
+  -- support for image pasting
+  'HakonHarnes/img-clip.nvim',
+  event = 'VeryLazy',
+  opts = {
+    -- recommended settings
+    default = {
+      embed_image_as_base64 = false,
+      prompt_for_file_name = false,
+      drag_and_drop = {
+        insert_mode = true,
+      },
+      -- required for Windows users
+      use_absolute_path = true,
+      only_render_image_at_cursor = true,
+    },
+  },
+  keys = {
+    -- suggested keymap
+    { '<leader>pi', '<cmd>PasteImage<cr>', desc = 'Paste image from system clipboard' },
+  },
+}
