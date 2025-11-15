8e3c18ee975456824e48680720995368ef94edab
Jonas Herrmansdsoerfer <jonas@herrmannsdoerfer.dev>
2025-11-15 18:13:24 +0100

    sketchybar: init

---
+++ sketchybar/install.sh
@@ -0,0 +1,9 @@
+brew install --cask font-hack-nerd-font
+
+brew tap FelixKratz/formulae
+brew install sketchybar
+
+# Install sketchybar-app-font for app icons
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/latest/download/sketchybar-app-font.ttf -o ~/Library/Fonts/sketchybar-app-font.ttf
+
+# https://felixkratz.github.io/SketchyBar/setup
