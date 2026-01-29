424a6785449ca425ae44dd47652c6fae8a3706c3
Jonas Herrmansdsoerfer <jonas@herrmannsdoerfer.dev>
2026-01-29 15:03:34 +0100

    leaderkey: add displayplacer dependency for presentation mode

--- leaderkey/install.sh
+++ leaderkey/install.sh
@@ -1,3 +1,8 @@
 brew install leader-key
 
 # https://github.com/mikker/LeaderKey.app
+
+# displayplacer for presentation mode resolution control
+brew install displayplacer
+
+# https://github.com/jakehilborn/displayplacer
