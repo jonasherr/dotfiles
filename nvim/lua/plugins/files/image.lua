5fe8fc766928785eaf8e8467a1867154d30a3367
Jonas Herrmansdsoerfer <jonas@herrmannsdoerfer.dev>
2026-02-20 19:37:10 +0100

    nvim: switch image rendering from 3rd/image.nvim to snacks.nvim

--- nvim/lua/plugins/files/image.lua
+++ nvim/lua/plugins/files/image.lua
@@ -1,25 +1,3 @@
-return {
-  '3rd/image.nvim',
-  build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
-  opts = {
-    processor = 'magick_cli',
-    backend = 'kitty',
-    integrations = {
-      markdown = {
-        enabled = true,
-        clear_in_insert_mode = false,
-        download_remote_images = true,
-        only_render_image_at_cursor = false,
-        only_render_image_at_cursor_mode = 'popup',
-        floating_windows = false, -- if true, images will be rendered in floating markdown windows
-        filetypes = { 'markdown', 'vimwiki' }, -- markdown extensions (ie. quarto) can go here
-      },
-      html = {
-        enabled = false,
-      },
-      css = {
-        enabled = false,
-      },
-    },
-  },
-}
+-- Image rendering now handled by snacks.nvim (see snacks.lua)
+-- Keeping img-clip.nvim for clipboard paste in image-clip.lua
+return {}
--- nvim/lua/plugins/files/snacks.lua
+++ nvim/lua/plugins/files/snacks.lua
@@ -2,6 +2,9 @@ return {
   'folke/snacks.nvim',
   ---@type snacks.Config
   opts = {
+    image = {
+      enabled = true,
+    },
     picker = {
       enabled = true,
     },
