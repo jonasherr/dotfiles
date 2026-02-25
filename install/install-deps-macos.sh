574c252cf97e6783714d9a6b4b2654f9ab990488
Jonas Herrmannsdörfer <jonas@herrmannsdoerfer.dev>
2026-02-25 07:20:14 +0100

    install: added whispering

--- install/install-deps-macos.sh
+++ install/install-deps-macos.sh
@@ -7,6 +7,7 @@ brew install --cask slack
 brew install --cask git-credential-manager
 brew install --cask whispering
 brew install oven-sh/bun/bun
+brew install --cask whispering
 
 brew install jesseduffield/lazygit/lazygit
 brew install jesseduffield/lazydocker/lazydocker
